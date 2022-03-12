442ee27fe2f36aae44143dc54e084bda7ce44479
yy-sky <1657615110@qq.com>
2022-03-12 18:54:46 +0800

    a

--- ls实现/my_ls.c
+++ ls实现/my_ls.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
 #include<sys/types.h>//opendir
 #include<dirent.h>//opendir,readdir
+#include<string.h>
+
+void do_ls(char dirname[]);
 
 int main (int argc,char* argv[])
 {
     if(argc == 1)
         do_ls(".");
     else
-        do_ls(*argv[1]);
+        do_ls(argv[1]);
     return 0;
 }
 
@@ -19,9 +22,13 @@ void do_ls(char dirname[])
         printf("my_ls：无法访问‘%s’：没有那个文件或目录\n",dirname);
     else
     {
-        while((direntp = readdir(dir) != NULL))
-            printf("%s\n",direntp->d_name);
-        clsodir(dir);
+        while((direntp = readdir(dir))!= NULL)
+        {
+            if(strcmp(direntp->d_name,".") != 0 && strcmp(direntp->d_name,"..") != 0 )
+                printf("%s  ",direntp->d_name);
+        }
+        printf("\n");
+        closedir(dir);
     }
 
 }
