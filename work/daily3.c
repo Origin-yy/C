d863387834f976a7294c2125c889dabca1b232fe
Origin_yy <1657615110@qq.com>
2022-03-30 21:07:37 +0800

    a

---
+++ work/daily3.c
@@ -0,0 +1,66 @@
+<html>
+
+<head>
+    <title>产品介绍</title>
+</head>
+
+<body>
+    <table width="650" border="1" height="50" cellspacing="0" cellpadding="0" align="center"
+        style="border-color:rgb(248, 150, 38)">
+        <tr>
+            <td width="200" height="200">
+                <img src="05.jpg" width="200" height="200">
+            </td>
+            <td style=background-color:#ebdaa4fa>
+                <b style=color:#f57c0b>产品名称：佳能 IXUS 130<br><br></b>
+
+                佳能 IXUS 130（官方标配）<br>
+                松下原装SD卡2G（高速正品）<br>
+                佳能IXUS系列专用皮包<br>
+                索尼2.7英寸LCD保护贴（防刮/高透光/静电吸附)<br>
+                摄影指南
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" width="500" height="10" style="background-color: coral;">
+                主要参数
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" width="600" height="100">
+                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<font style=color:#b1a0a0>型号:</font> PMP169B<br>
+                <font style=color:#b1a0a0>内存容量:</font> 512M<br>
+                <font style=color:#b1a0a0>屏幕尺寸:</font> 2.12英寸（最佳视觉比列16：9的宽屏）<br>
+                <font style=color:#b1a0a0>屏幕特性:</font> LTPS TFT (720x240)<br>
+                <font style=color:#b1a0a0>视频功能:</font> 支持ASF格式的MPG4,或通过软件转换成ASF播放,播放效果:320×420,30fps/视频<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp输出,输入/电视节目定时录制。
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" width="400" height="10" style="background-color: coral;">
+                功能参数
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" width="400" height="70">
+                <font style=color:#b1a0a0>音频功能:</font> 支持音频格式:MP3,WMA,WAV/内置麦克风,支持LINEIN寻录/收音功能/内置喇叭<br>
+                <font style=color:#b1a0a0>录音格式:</font> 44.1KHz, 128 Kbps, MP3, 支持MIC/LINE-IN直录<br>
+                <font style=color:#b1a0a0>附加功能:</font> 支持图片格式:JPEG(EXIF2.1)/电子书浏览/多语言(中/英)设置
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" width="400" height="30" style="background-color: coral;">
+                其他参数
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" width="50" height="100">
+                <font style=color:#b1a0a0>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp接口:</font> USB接口,AV OUT接口, AV IN接口<br>
+                <font style=color:#b1a0a0>&nbsp&nbsp&nbsp&nbsp&nbsp扩展卡:</font> 可扩充2G SD,MMC卡<br>
+                <font style=color:#b1a0a0>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp电池:</font> 内置锂电池<br>
+                <font style=color:#b1a0a0>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp尺寸:</font> 105.2 x47.6x15.6mm<br>
+                <font style=color:#b1a0a0>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 重量:</font> 90g<br>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
