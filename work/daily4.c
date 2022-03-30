27b40b4cd62a71e4785abe48fb228eced13104c9
Origin_yy <1657615110@qq.com>
2022-03-30 21:07:37 +0800

    a

---
+++ work/daily4.c
@@ -0,0 +1,99 @@
+<html>
+
+<head>
+    <title>留言簿</title>
+</head>
+
+<body>
+    <form action="https://pintia.cn/problem-sets?tab=1">
+        <table width="800" border="1" align="center" style="background-color:#ece8e8">
+            <tr>
+                <td colspan="2" align="center" style="background-color: rgb(194, 186, 238);">
+                    <b>留言簿</b>
+                </td>
+            </tr>
+            <tr>
+                <td align="right" width="170">
+                    您的姓名:&nbsp&nbsp&nbsp
+                </td>
+                <td>
+                    <input type="text">
+                    <font style="color:#f00c0c">*</font>
+                </td>
+            </tr>
+            <tr>
+                <td align="right">
+                    您的邮箱:&nbsp&nbsp&nbsp
+                </td>
+                <td>
+                    <input type="text">
+                    <font style="color:#f00c0c">*</font>
+                </td>
+            </tr>
+            <tr>
+                <td align="right" width="170">
+                    您的网站:&nbsp&nbsp&nbsp
+                </td>
+                <td>
+                    <input type="text" value="http://">
+                </td>
+            </tr>
+            <tr>
+                <td align="right" width="170">
+                    其他联系方式:&nbsp&nbsp&nbsp
+                </td>
+                <td>
+                    <input type="text">
+                    <font style="color:#f00c0c">*</font>（如qq、MSN、电话等）
+                </td>
+            </tr>
+            <tr>
+                <td align="right">
+                    留言内容:&nbsp&nbsp&nbsp<br>
+                    <font style="color:#f00c0c">(200字以内）</font>
+                </td>
+                <td height="110">
+                    <input type="text" style="height: 150px;width:550px">
+                    <font style="color:#f00c0c">*</font>
+                </td>
+            </tr>
+            <tr>
+                <td align="right" width="170">
+                    请选择表情:&nbsp&nbsp&nbsp
+                </td>
+                <td height="80">
+                    <input type="radio"><img src="face1.gif"> <input type="radio"><img src="face2.gif"> <input
+                        type="radio"><img src="face3.gif"> <input type="radio"><img src="face4.gif"> <input
+                        type="radio"><img src="face5.gif"> <input type="radio"><img src="face6.gif"> <input
+                        type="radio"><img src="face7.gif"> <input type="radio"><img src="face8.gif"> <input
+                        type="radio"><img src="face9.gif"> <input type="radio"><img src="face10.gif"><br>
+                    <input type="radio"><img src="face11.gif"> <input type="radio"><img src="face12.gif"> <input
+                        type="radio"><img src="face13.gif"> <input type="radio"><img src="face14.gif"> <input
+                        type="radio"><img src="face15.gif"> <input type="radio"><img src="face16.gif"> <input
+                        type="radio"><img src="face17.gif"> <input type="radio"><img src="face18.gif"> <input
+                        type="radio"><img src="face19.gif"> <input type="radio"><img src="face20.gif">
+                </td>
+            </tr>
+            <tr>
+                <td align="right" width="170">
+                    请选择头像:&nbsp&nbsp&nbsp
+                </td>
+                <td height="160">
+                    <input type="radio"><img src="pic1.gif">&nbsp&nbsp&nbsp&nbsp <input type="radio"><img
+                        src="pic2.gif"> <input type="radio"><img src="pic3.gif"> <input type="radio"><img
+                        src="pic4.gif"> <input type="radio"><img src="pic5.gif"><br>
+                    <input type="radio"><img src="pic6.gif"><input type="radio"><img src="pic7.gif"><input
+                        type="radio"><img src="pic8.gif"><input type="radio"><img src="pic9.gif"><input
+                        type="radio"><img src="pic10.gif">
+                </td>
+            </tr>
+            <tr>
+                <td align="center" colspan="2" height="29">
+                    <input type="submit" value="提交留言">&nbsp&nbsp&nbsp<input type="reset" class="button" value="重新填写">
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+
+</html>
