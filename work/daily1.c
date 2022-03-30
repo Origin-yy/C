f4f6e4507d9ca4bbfe13cb6f150c494cb09fa19a
Origin_yy <1657615110@qq.com>
2022-03-30 16:49:24 +0800

    a

---
+++ work/daily1.c
@@ -0,0 +1,105 @@
+<html>
+<head>
+<title> 调查</title>
+<style type="text/css";
+<!--
+.tt1
+{
+    font-size: 14px;
+    color: #FFFFFF;
+}
+.bd1
+{
+    border:1px solid #F6600;
+}
+.bd2
+{
+    border-right-width:1px;
+    border-bottom-width:1px;
+    border-top-style: none;
+    border-right-style: solid;
+    border-bottom-style: solid;
+    border-left-style: none;
+    border-right-color:#CCD8E6;
+    border-bottom-color.#CCD8E6;
+    font-size: 13px;
+}
+.bd4
+{
+    border-bottom-width: 1px;
+    border-bottom-style: solid;
+    border-bottom-color.#CCD8E6;
+    font-size:13px;
+}
+.tt2
+{
+    font-size:13px;
+}
+-->
+</style>
+</head>
+<body>
+<table width="450" border="0" align="center" cellpadding="0" cellspacing="0">
+<tr>
+<td><fom action="" method="post" name="form1" id="form1">
+<table width="100%" border="0" cellpadding="5" cellspacing="0"class="bd1">
+<tr>
+<td colspan="2" bgcolor="#F6600" class="t1"> 同城网在线调查</td>
+</tr>
+<tr>
+<td width="38%" class="bd2"> 你从哪里知道同城的 </td>
+<td width="62%" class="bd4"><select name="select">
+<option value="1"> 网上搜索 </option>
+<option value="2"= 朋友介绍 </option>
+<option value="3"= 广告宣传 </option>
+<option value="4"= 报刊杂志 </option>
+<option value="5"> 其他渠道 </option>
+</select>
+</td>
+</tr>
+<td class="bd2"> 你正在使用的同城网服务 </td>
+<td class="bd4"> <input name="f1"type="checkbox" id="f1" value="社区"/>
+<span class="t2"> 免费社区 </span>
+<input name="f2" type="checkbox" id="f2" value=" 留言"/>
+<span class="t2">
+免费留言簿
+<input name="f3" type="checkbox" id="f3" value="投票"/>
+</span> <span class="tt2"> 免费投票
+</span></td>
+</tr>
+<td class="bd2"> 你希望我们提供哪些新服务 <itd>
+<td class="bd4"><input name="nf1" type="checkbox" id="nf1"value="1"/>
+聊天室
+<input name="nf2" type="checkbox" id="nf2" value="2" />
+博客
+<input name="nf3" type="checkbox" id="nf3" value="3" />
+游戏系统 </td>
+</tr>
+<tr>
+<td class="bd2"> 你是同城网的会员吗 </td>
+<td class="bd4">
+<label>
+<span class="tt2">
+<input type="radio" name="huiyuar" value="1" />
+是
+</span></label><span class="tt2"> <label>
+<input type="radio" name="huiyuar" value="0" />
+不是</labe>
+</span></td>
+</tr>
+<tr>
+<td valign="top" class="bd2"> 你对同城网的其他意见</td>
+<td class="bd4"><textarea name="yijian" cols="30" rows="10"wrap="physical" id="yijian"></textarea></td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td><input type="submit" name="Submit" value="提交"/><td>
+</tr>
+</table>
+</form>
+</td>
+</tr>
+</table>
+</body>
+</html>
+
