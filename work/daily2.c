7bfc6e4e8fec282eec8f30a631a38282452c4cdc
Origin_yy <1657615110@qq.com>
2022-03-31 16:14:01 +0800

    a

--- work/daily2.c
+++ work/daily2.c
@@ -1,75 +1,22 @@
-<html>
-<head>
-<title>成绩登记表</title>
-<style type="text/css">
-#score{width:600px;border: 1px solid #000000;border-collapse: collapse;}
-#score th,td {border:1px solid #000000:}
-#score  caption
-{font-size:28px;font-weight:bold;letter-spacing:12px:line-height:2.5;}
-#score  th  {background-color:#4682B4;font-size:20px;font-weight:bold:line-hei ght:2}
-#score td {font-size:18px:line-height:2;text-align:center;}
-#score.seq {width: 50px;}
-#score .studentId {width: 120px;}
-#score .studentName {width:80px;}
-#score .hwScore {width: 100px:}
-#score .termScore {width: 100px;}
-#score .finalScore {width:120px;}
-/*斑马纹类*/
-.odd td {background-color:#FFF;}
-.even td {background-color:#CCCCCC;}
-</style>
-</head>
-<body>
-<table id="score">
-<caption>
-成绩登记表
-</caption>
-<tr>
-<th olass="seq">序号</th>
-<th class="studentId">学号</th>
-<th class="studentName">姓名</th>
-<th class="hwScore">平时成绩</th>
-<th class="termScore">期末成绩</th>
-<th class="finalScore">学期总成绩</th>
-</tr>
-<tr class="odd">
-<td>1</td>
-<td>2010300201</td>
-<td>张小丽</td>
-<td>95</td>
-<td>95</td>
-<td>95</td>
-</tr>
-<tr class="even">
-<td>2</td>
-<td>2010300202</td>
-<td>李宁</td>
-<td>90</td>
-<td>88</td>
-<td>89</td>
-</tr>
-<tr class="odd">
-<td>3</td>
-<td>2010300203</td>
-<td>刘梅</td>
-<td>98</td>
-<td>92</td>
-<td>95</td>
-</tr>
-<tr class="even">
-<td>4</td>
-<td>2010300204</td>
-<td>王刚</td>
-<td>98</td>
-<td>90</td>
-<td>94</td>
-</tr>
-<tr class="odd">
-<td>5</td>
-<td>2010300205</td>
-<td>郑军</td>
-<td>90</td>
-<td>85</td>
-<td>87</td>
-</tr>
-<tr class="even">
+#include <stdio.h>
+int main(int argc, char** argv)
+{
+    double s;
+    double t=1;
+    int n = 1;
+    while (1/t > 0.000001)
+    {
+        t=1;
+        for(int i=1;i<=n;i++)
+        {
+            t *= i;
+        }
+        if(n%2 == 1)
+            s += 1/t;
+        else
+            s -= 1/t;
+        n++;
+    }
+    printf("S = %lf.\n",s);
+    return 0;
+}
