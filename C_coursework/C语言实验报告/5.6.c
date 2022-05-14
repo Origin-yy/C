5e8684fe02af4c510007f9c610a7e0ee542222a1
Origin_yy <1657615110@qq.com>
2022-05-14 22:16:44 +0800

    a

---
+++ C_coursework/C语言实验报告/5.6.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void input (int a[],int b[]);
+void merge (int a[],int b[],int c[]);
+void put (int c[]);
+
+int main(void)
+{
+    
+    return 0;
+}
+void input (int a[],int b[])
+{
+
+}
+void merge (int a[],int b[],int c[])
+{
+
+}
+void put (int c[])
+{
+
+}
