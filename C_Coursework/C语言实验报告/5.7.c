03bb48735bfb324f1517aac0ab470fd947408161
Origin_yy <1657615110@qq.com>
2022-05-20 18:09:38 +0800

    a

--- C_Coursework/C语言实验报告/5.7.c
+++ C_Coursework/C语言实验报告/5.7.c
@@ -1,23 +1,41 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
 int cmp(const void*a,const void*b)
 {
     return strcmp((char*)a,(char*)b);
 }
-int main(void)
+
+void input(char a[][100])
 {
-    char a[6][100]={0};
-    printf("请输入6个字符串:\n");
+	printf("请输入6个字符串:\n");
     for(int i=0; i<6; i++)
     {
         scanf("%s",a[i]);
     }
-    qsort(a,6,100,cmp);
+}
+
+void sort(char a[][100])
+{
+	qsort(a,6,100,cmp);
+}
+
+void output(char a[][100])
+{
     printf("按字典序排序后为:\n");
     for(int i=0; i<6; i++)
     {
         printf("%s\n",a[i]);
     }
+}
+
+int main(void)
+{
+    char a[6][100]={0};
+	input(a);
+	sort(a);
+	output(a);
+
     return 0;
 }
