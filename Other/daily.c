e8bf22b59553126ce42afee3561f91fd239c4f5c
Origin-yy <1657615110@qq.com>
2022-06-08 10:24:39 +0800

    a

--- Other/daily.c
+++ Other/daily.c
@@ -1,52 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
-typedef struct node
+#include<stdlib.h>
+
+struct Emp
 {
-	int date;
-	struct node* next;
-}node,*list;
-void suan(list a,int*max,int*min,double*average);
+	char ID[10];//职工工号
+	char Name[10];//职工姓名
+	int age;//职工年龄
+};
+
 int main()
 {
-	int x,max,min;
-	double average;
-	list head=(list)malloc(sizeof(node));
-	head->next=NULL;
-	list p=NULL,q;
-	q=head;
-	do{
-		list p=(list)malloc(sizeof(node));
-		scanf("%d",&x);
-		p->date=x;
-		p->next=q->next;
-		q->next=p;
-		q=p;
-	}while(p&&getchar()!='\n');
-	suan(head,&max,&min,&average);
-	printf("最大值为%d\n最小值为%d\n平均值为%lf\n",max,min,average);
+	/***************found***************/
+	struct Emp e[5] = { {"20110102","Kong",36},
+					{"20081025","song",41},
+					{"19880722","chai",53},
+					{"19961210","zhang",48},
+					{"19900311","Gong",51} };
+	int cnt  = 0;
+	int i = 0;
+
+	for (i = 0;i < 5;i++)
+		/***************found***************/
+		if (e[i].age<45)
+		{
+			printf("工号：%s\t姓名：%s\t年龄：%d\n", e[i].ID, e[i].Name, e[i].age);
+			cnt++;
+		}
+	printf("年龄在45岁以下的职工数为：%d\n", cnt);
+
+	system("pause");
 	return 0;
-}
-void suan(list a,int*max,int*min,double*average)
-{
-	int count=0;
-	list p=a->next;
-	*max=p->date;
-	*min=p->date;
-	double sum=0.0;
-	while(p){
-		if(*max<p->next->date){
-		   *max=p->next->date;
-		   p=p->next;
-	    }
-	    if(*min>p->next->date){
-		   *min=p->next->date;
-		   p=p->next;
-	    }
-	}
-    while(p){
-    	sum+=p->date;
-    	count++;
-    	p=p->next;
-	} 
-	*average=sum/count;
 }
