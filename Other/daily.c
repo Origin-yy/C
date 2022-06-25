ffd8a72f1a1414156da0949d11a3d10aa55f0fb9
Origin-yy <1657615110@qq.com>
2022-06-25 20:48:12 +0800

    a

--- Other/daily.c
+++ Other/daily.c
@@ -1,9 +1,39 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+ 
+void handler(int s)
 {
-    int i = 0;
-    int j =0;
-    i = 3;
-    j = (++i) + (++i) * (++i);
-    printf("i:%d,j:%d\n",i,j);
+	printf("信号到达。\n");
+	return;
+}
+
+int readn(int fd, void* buf,int n)
+{
+	struct sigaction act;  
+	act.sa_handler = handler;  
+	sigemptyset(&act.sa_mask);  
+	act.sa_flags = 0;  
+	act.sa_flags |= SA_INTERRUPT;  //设置该选线系统调用不会自动重启
+	if(sigaction(SIGALRM, &act, NULL) == -1)  
+	{
+		perror("sigaction");
+		exit(-1);
+	}
+	
+	alarm(5);
+	read(0, buf, sizeof(buf));  //从标准输入读取字符
+
+	return 0;
+}
+
+int main()
+{	
+	char buf[10];
+
+	readn(0, buf, sizeof(buf));  //从标准输入读取字符
+	printf("read()超时返回\n");
+
+    return 0;
 }
