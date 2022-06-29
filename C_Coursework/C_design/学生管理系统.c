71c97f62359b17fd342f09e30a0156b296b43486
Origin-yy <1657615110@qq.com>
2022-06-29 16:07:36 +0800

    a

--- C_Coursework/C_design/学生管理系统.c
+++ C_Coursework/C_design/学生管理系统.c
@@ -7,26 +7,25 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define NONE "\e[0m"
-#define BLACK "\e[0;30m"
-#define L_BLACK "\e[1;30m"
-#define RED "\e[0;31m"
-#define L_RED "\e[1;31m"
-#define GREEN "\e[0;32m"
-#define L_GREEN "\e[1;32m"
-#define YELLOW "\e[0;33m"
-#define L_YELLOW "\e[1;33m"
-#define BLUE "\e[0;34m"
-#define L_BLUE "\e[1;34m"
-#define PURPLE "\e[0;35m"
-#define L_PURPLE "\e[1;35m"
-#define D_GREEN "\e[0;36m"
-#define L_D_GREEN "\e[1;36m"
-#define WHITE "\e[0;37m"
-#define L_WHITE "\e[1;37m"
+#define NONE        "\e[0m"
+#define BLACK       "\e[0;30m"
+#define L_BLACK     "\e[1;30m"
+#define RED         "\e[0;31m"
+#define L_RED       "\e[1;31m"
+#define GREEN       "\e[0;32m"
+#define L_GREEN     "\e[1;32m"
+#define YELLOW      "\e[0;33m"
+#define L_YELLOW    "\e[1;33m"
+#define BLUE        "\e[0;34m"
+#define L_BLUE      "\e[1;34m"
+#define PURPLE      "\e[0;35m"
+#define L_PURPLE    "\e[1;35m"
+#define D_GREEN     "\e[0;36m"
+#define L_D_GREEN   "\e[1;36m"
+#define WHITE       "\e[0;37m"
+#define L_WHITE     "\e[1;37m"
 
-#define FILE_PATH                                                              \
-  "/home/yuanye/my_code/my_code.c/C_Coursework/C_design/学生信息.c"
+#define FILE_PATH   "/home/yuanye/my_code/my_code.c/C_Coursework/C_design/学生信息.c"
 
 typedef struct Node {
   int id;
@@ -75,17 +74,19 @@ void write_information(List L);
 void destroy(List L);
 
 int main(void) {
+  printf(L_RED "欢迎使用学生管理系统!\n\n");
   int flag = -1;
   List L = List_init();
 
   display_options();
+  printf("\n");
 
   while (1) {
-    printf("请输入您想进行的操作的序号: \n");
+    printf(L_BLUE"请输入您想进行的操作的序号: \n"NONE);
     scanf("%d", &flag);
     switch (flag) {
     case 0:
-      printf("感谢您的使用，再见。\n");
+      printf(L_YELLOW"期待您的下次使用，再见!\n"NONE);
       exit(0);
     case 1:
       Node_insert(L);
@@ -113,6 +114,7 @@ int main(void) {
       break;
     case 9:
       display_options();
+      break;
     default:
       printf(L_RED "输入有误，请重新输入。\n" NONE);
       continue;
@@ -199,6 +201,7 @@ void Node_insert(List L) {
     p = T;
   }
   L->id += n;
+  printf(L_GREEN"%d名学生信息已录入完毕.\n"NONE,n);
 }
 
 //删除学生信息
@@ -224,11 +227,54 @@ void Node_delete(List L) {
     }
   }
   if (flag == 0)
-    printf(L_RED "没有该学生，请确认后重试.\n" NONE);
+    printf(L_RED "\n没有该学生,请确认后重试.\n" NONE);
   else {
-    printf(L_GREEN "%s的学生信息已删除.\n" NONE, name);
+    printf(L_GREEN "\n%s的学生信息已删除.\n" NONE, name);
     L->id--;
   }
+}
+
+//查询学生信息
+void Node_find(List L) {
+  if (L->next == NULL) {
+    printf(L_RED "当前还没有学生信息,请确认后重试.\n" NONE);
+    return;
+  }
+  char name[50];
+  int flag = 0;
+  int i = 1;
+
+  printf("请输入您想查询的学生姓名：");
+  scanf("%s", name);
+
+  Node *p = L;
+  while (p->next) {
+    p = p->next;
+    if (strcmp(p->name, name) == 0) {
+      printf("\n学生%s的信息如下: \n", name);
+      printf(L_YELLOW "********************************************************"
+                      "*****************\n" NONE);
+      printf(L_BLUE "*   序号|      学号|    姓名|   性别|   语文|   数学|   "
+                    "英语|   均分|   *\n" NONE);
+      printf(L_YELLOW "*" NONE);
+      printf("%7d|", i);
+      printf("%10d|", p->id);
+      printf("%8s|", p->name);
+      printf("%7c|", p->sex);
+      printf("%7d|", p->Chinese);
+      printf("%7d|", p->Math);
+      printf("%7d|", p->English);
+      printf("%7.3lf|", (double)(p->Chinese + p->Math + p->English) / 3);
+      printf(L_YELLOW "   *\n" NONE);
+      printf(L_YELLOW
+             "************************************************************"
+             "*************\n" NONE);
+      flag = 1;
+      break;
+    }
+  }
+  if (flag == 0)
+    printf(L_RED "没有该学生，请确认后重试.\n" NONE);
 }
 
 //修改学生信息
@@ -237,14 +283,20 @@ void Node_change(List L) {
     printf(L_RED "当前还没有学生信息,请确认后重试.\n" NONE);
     return;
   }
+
   char name[50];
   int flag = 0;
+
   printf("请输入您想修改的学生的姓名：");
   scanf("%s", name);
+
   Node *p = L;
   while (p->next) {
     p = p->next;
+
     if (strcmp(p->name, name) == 0) {
+      flag = 1;
+
       printf("学号:");
       while (!scanf("%d", &p->id) || p->id > 100000000 || p->id < 1) {
         printf(L_RED "学号范围是1-10000000,请重新输入.\n" NONE);
@@ -299,49 +351,6 @@ void Node_change(List L) {
     printf(L_GREEN "%s的学生信息已修改完毕.\n" NONE, name);
 }
 
-//查询学生信息
-void Node_find(List L) {
-  if (L->next == NULL) {
-    printf(L_RED "当前还没有学生信息,请确认后重试.\n" NONE);
-    return;
-  }
-  char name[50];
-  int flag = 0;
-  int i = 1;
-
-  printf("请输入您想查询的学生姓名：");
-  scanf("%s", name);
-
-  Node *p = L;
-  while (p->next) {
-    p = p->next;
-    if (strcmp(p->name, name) == 0) {
-      printf("学生%s的信息如下: \n", name);
-      printf(L_YELLOW "********************************************************"
-                      "*****************\n" NONE);
-      printf(L_BLUE "*   序号|      学号|    姓名|   性别|   语文|   数学|   "
-                    "英语|   均分|   *\n" NONE);
-      printf(L_YELLOW "*" NONE);
-      printf("%7d|", i);
-      printf("%10d|", p->id);
-      printf("%8s|", p->name);
-      printf("%7c|", p->sex);
-      printf("%7d|", p->Chinese);
-      printf("%7d|", p->Math);
-      printf("%7d|", p->English);
-      printf("%7.3lf|", (double)(p->Chinese + p->Math + p->English) / 3);
-      printf(L_YELLOW "   *\n" NONE);
-      flag = 1;
-      break;
-    }
-  }
-  if (flag == 0)
-    printf(L_RED "没有该学生，请确认后重试.\n" NONE);
-  else {
-    printf(L_GREEN "%s的学生信息已删除.\n" NONE, name);
-    L->id--;
-  }
-}
 
 //排序学生信息
 void List_sort(List L) {
@@ -351,7 +360,7 @@ void List_sort(List L) {
     return;
   }
   if (L->next->next == NULL) {
-    printf("当前只有一个学生信息,无需排序.\n\n");
+    printf(L_RED"当前只有一个学生信息,无需排序.\n\n"NONE);
     return;
   }
   a = L->next;
@@ -371,11 +380,16 @@ void List_sort(List L) {
     }
     a = a->next;
   }
-  printf(L_GREEN "已按照三门课程的平均成绩对学生进行排序\n\n" NONE);
+  printf(L_GREEN "已按照三门课程的平均成绩对学生进行排序\n" NONE);
 }
 
 //展示学生信息
 void List_output(List L) {
+  if(L->id == 0)
+  {
+    printf(L_RED"当前没有学生信息，请确认后重试.\n"NONE);
+    return;
+  }
   Node *p = L;
   int i = 1;
   printf("已有的学生信息如下:\n\n");
@@ -397,11 +411,12 @@ void List_output(List L) {
     printf(L_YELLOW "   *\n" NONE);
     i++;
   }
-  printf(L_GREEN "已录入的学生信息展示完毕.\n" NONE);
+  printf(L_YELLOW "************************************************************"
+                  "*************\n" NONE);
+  printf(L_GREEN "\n已录入的学生信息展示完毕.\n" NONE);
 }
 
 void display_options() {
-  printf(L_RED "欢迎使用学生管理系统!。\n\n");
   printf(
       L_YELLOW
       "************************学生管理系统*************************\n" NONE);
@@ -501,7 +516,7 @@ void write_information(List L) {
 
   fd = open(path, O_RDWR);
   if (fd < 0)
-    printf(L_RED "没有找到文件%s.\n" NONE, path);
+    printf(L_RED "没有找到文件%s.\n\n" NONE, path);
   else
     truncate(path, 0);
   ;
@@ -523,7 +538,7 @@ void write_information(List L) {
     i++;
   }
   close(fd);
-  printf(L_GREEN "学生信息已保存到%s" NONE, path);
+  printf(L_GREEN "学生信息已保存到%s\n" NONE, path);
 }
 //销毁链表
 void destroy(List L) {
