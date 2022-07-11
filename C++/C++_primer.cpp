66c5670aee27660f95ea8898bafd561f8a51feb2
Origin-yy <1657615110@qq.com>
2022-07-11 22:13:28 +0800

    a

--- C++/C++_primer.cpp
+++ C++/C++_primer.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <cctype>
 extern const int i = 123; // a文件里定义并初始化i，加extern使其能被其他文件使用
 
 using namespace std; //头文件不应包含using声明（名字冲突）
@@ -31,6 +31,7 @@ int main() {
     int id = 0;
   };
   sale a,b,c;
+
   // string 初始化
   string s;
   string *ptr = nullptr; //不能把int类型变量赋给指针，即使值为0。
@@ -42,32 +43,46 @@ int main() {
   // string s4(s2);
   string s5 = s1;
   // string操作
-  s1 + s2;
-  // s1 == s2;
-  int num = s.size();
-
-  cout << s1;
-  cin >> s2;
-  getline(cin, s1);
-  bool if_empty = s.empty();
-  // int num = s.size();
-  cin >> s;
-  //当读取的不是一个字符串，或者遇到文件结尾，就退出循环
+  if(s1 == s2 || s1.empty() || s2.size() == 0 || s2[4] == 'c')
+  {
+    getline(cin,s);
+    string s7 = s1 + "asdf"; //每个+号两侧的运算对象至少有一个是string
+    cout << s1 << endl;
+    cin >> s2;
+    bool if_empty = s1.empty();
+  }
+  //反复读取，直到遇到文件结尾
   while (cin >> s)
     cout << s << endl;
-  //读取一行输入，直到回车，且不包含回车
+  //读取一行输入,读取\n并丢弃，并且终止输入
   string line;
-  while (getline(cin, line))
+  while (getline(cin, line))//读取一行，包括空白符
     cout << line << endl;
-  // auto line = line.size();
-  // //auto类型，让编译器来推断类型，这里是string::size_type;
+  auto line_t = line.size();
+  // auto类型，让编译器来推断类型，这里是string::size_type;
   // s.size()的返回值是一个无符号整数，不要与int相比
   int n = -1; //有符号和无符号在一起，都转为无符号
-  int t = s.size() < n; //几乎肯定为true，负值n会转为一个比较大的无符号值
+  //int t = s.size() < n; //几乎肯定为true，负值n会转为一个比较大的无符号值
 
-  // vector末班
-  vector<int> ivec;
-  vector<vector<string>> file;
+  //范围for语句， 想要修改string中字符的值，要用引用
+  for(auto &c :s1){
+    c = toupper(c);
+  }
+  string::size_type index = 0;//下标类型，无符号整数
+
+  //decltype关键字
+  decltype(s.size()) cnt = 0; //根据（）里变量的类型为类型
+
+  // vector模板
+  vector<int> ivec(10,2);
+  vector<int> svec(10);  //十个元素，每个初始化为0，值初始化，各种初始化详见p89
+  vector<vector<string> > file{{"asdf"," qwer","qsfe"},{"esed"}};
+  //.push_back()把元素放到vector尾端
+  vector<string> words;
+  for(int i = 0; i<10; i++){
+    string buf = "asdf";
+    words.push_back(buf);
+  }
 
   return 0;
 }
