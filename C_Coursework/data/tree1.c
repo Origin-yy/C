bfbfa842dc2b598141e6bd1bd4cd436ef7e80b75
Origin-yy <1657615110@qq.com>
2022-11-09 22:24:15 +0800

    a

--- C_Coursework/data/tree1.c
+++ C_Coursework/data/tree1.c
@@ -1,6 +1,8 @@
+#include "output_control.h"
 #include <stdio.h>
 #include <malloc.h>
 #include <limits.h>
+#include <math.h>
 
 // ABC##DE#G##F###
 #define MAXSIZE 10000
@@ -35,12 +37,18 @@ ElemType* GetTop(SeqStack* s);       // 取栈顶元素
 int NonLeafNode(BiTree root);        // 统计二叉树中非叶子结点的个数  
 int BiTreeDeepth(BiTree root);       // 计算二叉树的高度  
 void CountLeaf(BiTree root,int *Count);// 统计二叉树中叶子节点的个数
+int Traverse_R(BiTree T, int depth, int right, int tap); // 树状打印二叉树
+void gotoxy(int x, int y);                               // 移动光标
+int BreakBiTree(BiTree T, BiTree* L, BiTree* R);           // 将二叉树分为根，左子树，右子树三个部分
 
 int main(void) {
     int leaf_num = 0;
     printf("请按先序输入二叉树，‘#’表示节点为空： \n");
-    BiTree root = NULL;
-    root = CreateTree(root);
+    BiTree tmp = NULL;
+    BiTree root = CreateTree(tmp);
+
+    Traverse_R(root, 0, 0, 0);
+    printf("\n");
 
     printf("先序遍历结果为：");
     PreOrder(root);
@@ -193,3 +201,88 @@ int BiTreeDeepth(BiTree root) {
     }
 }
 
+/*
+* 递归打印打印出树形
+* T		正在打印的树
+* depth	目前在打印树的第几层
+* right	该子树是否为右子树
+* tap	目前子树需要的相对偏移数量
+*/
+int Traverse_R(BiTree T, int depth, int right, int tap) {
+	if (T == NULL) return 1;
+
+	// 获取一次树的初始高度，用于计算相对偏移数量
+	int treeDepth = BiTreeDeepth(T);
+	// 记录当前光标位置，用于在递归中记录当前递归时树的位置
+	int x, y;
+	// 拆分树，将树的左右子树拆分开来
+	BiTree L = NULL, R = NULL;
+	BreakBiTree(T, &L, &R);
+
+	// 计算父树的偏移量
+	int tap1 = tap * pow(2, treeDepth - depth);
+	// 计算子树的偏移量
+	int tap2 = right * (pow(2, treeDepth - depth));
+	// 计算半偏移量
+	int tap3 = pow(2, treeDepth - depth - 1);
+
+	// 获取根的坐标
+	// x 计算方法为：父偏移量 + 子偏移量 + 半偏移量 - 1
+	// y 计算方法为：目前打印的层数 * 2
+	x = tap1 + tap2 + tap3 - 1;
+	y = depth * 2;
+
+	// 打印根的位置
+	gotoxy(x * 2, y);
+	printf("%c", T->data);
+
+	// 在打印子树时，当前层数+1
+	depth++;
+	// 计算子树的父偏移量
+	tap = tap * 2 + (right == 1 ? 2 : 0);
+	if (L == NULL && R == NULL) return 1;
+	else if (R == NULL) {
+		// 打印左子树的位置
+		gotoxy(x * 2 - tap3, y + 1);
+		printf("┏");
+		for (int i = 0; i < tap3-1; i++) printf("━");
+		printf("┛");
+		Traverse_R(L, depth, 0, tap);
+	}
+	else if (L == NULL) {
+		// 打印右子树的位置
+		gotoxy(x * 2, y + 1);
+		printf("┗");
+		for (int i = 0; i < tap3-1; i++) printf("━");
+		printf("┓");
+		Traverse_R(R, depth, 1, tap);
+	}
+	else {
+		// 打印左右子树的位置
+		gotoxy(x * 2 - tap3, y + 1);
+		printf("┏");
+		for (int i = 0; i < tap3 - 1; i++) printf("━");
+		printf("┻");
+		for (int i = 0; i < tap3 - 1; i++) printf("━");
+		printf("┓");
+		Traverse_R(L, depth, 0, tap);
+		Traverse_R(R, depth, 1, tap);
+	}
+    return 0;
+}
+
+//改变光标位置
+void gotoxy(int x, int y){
+    MOVELEFT(100);
+	MOVERIGHT(x);
+    MOVEDOWN(y);
+}
+
+// 将二叉树分为根，左子树，右子树三个部分
+int BreakBiTree(BiTree T, BiTree* L, BiTree* R) {
+	if (T == NULL) return 1;
+	*L = T->LChild;
+	*R = T->RChild;
+
+	return 1;
+}
